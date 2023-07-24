64c581b68b535833b5db83e414ed4941a897fad7
Animesh Acharya <animeshacharya867+github@gmail.com>
2023-07-24 20:48:50 +0545

    Changed files

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SearchBar() {
+  return (
+    <div>
+      <div className="h-2 w-6/12 backdrop-blur-md bg-cyan-300">hello</div>
+    </div>
+  );
+}
